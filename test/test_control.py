10cf6935cfe1446871ec0b62d91d7157ba69418e
Katherine Gallagher <katherinegallagher@fas.harvard.edu>
2017-08-02 23:04:57 -0400

    tests through step function

--- test/test_control.py
+++ test/test_control.py
@@ -16,5 +16,38 @@ class TestControl(unittest.TestCase):
 		config.initialize_root_node(*root_node_data)
 		config.set_exit_node(*exit_node_data)
 
+	def test_run(self):
+		control._step_function = MagicMock()
+		control.run()
+
+		self.assertTrue(control._step_function.called)
+
+	def test_step_function(self):
+		control._net_function = MagicMock()
+		control._link_function = MagicMock()
+		control._link_function.return_value = "OUTPUT"
+		control._zero_gates = MagicMock()
+
+		self.assertEqual(control._step_function(), "OUTPUT")
+		self.assertTrue(control._net_function.called)
+		self.assertTrue(control._link_function.called)
+		self.assertTrue(control._zero_gates.called)
+
+	def test_net_function(self):
+		for node in Nodenet.Instance().node_dict.values():
+			node.node_function = MagicMock()
+			for slot in node.slot_vector:
+				slot.activation = 0
+
+		control._net_function()
+		for node in Nodenet.Instance().node_dict.values():
+			self.assertFalse(node.node_function.called)
+
+	# def tearDown(self):
+	# 	node_dict = Nodenet.Instance().node_dict
+
+	# 	for key in node_dict.keys():
+	# 		node_dict.pop(key)
+
 if __name__ == '__main__':
 	unittest.main()
