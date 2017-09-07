03e08faf42577f0c63171d8064fbc89fedc6ed5f
kgallagher <katherinevgallagher@gmail.com>
2017-09-07 16:15:25 -0400

    code cleanup

--- nodenet/config.py
+++ nodenet/config.py
@@ -7,65 +7,28 @@ from .nodenet import Nodenet
 # NODES
 
 def generate_node_data(network_dimensions):
-	node_data = []
-	for layer_index, num_nodes in enumerate(network_dimensions):
-		layer = [["layer"+str(layer_index)+"node"+str(n), "register"] for n in range(0, num_nodes)]
-		node_data.append(layer)
-	return node_data
+	return [[["layer"+str(layer_index)+"node"+str(n), "register"] 
+	for n in range(0, num_nodes)] for layer_index, num_nodes in enumerate(network_dimensions)]
 
 def add_nodes(nodenet, node_data):
 	nodenet.add_layers(node_factory(node_data))
 
 def remove_nodes(nodenet, node_data):
 	node_dict = nodenet.node_dict
-
-	for name in node_data:
-		node_dict.pop(name)
+	[node_dict.pop(name) for name in node_data]
 
 # LINKS
 
 def generate_link_data(nodenet):
-	link_data = []
-	
-	for layer_index, layer in enumerate(nodenet.layers):
-
-		# don't need links for the last layer of nodes
-		if 0 < layer_index < len(nodenet.layers):
-			layer_links = []
-
-		 	for target_node in layer:
-		 		node_links = []
-
-		 		for origin_node in nodenet.layers[layer_index-1]:
-
-			 		node_links.append({
-			 			"origin": [origin_node.name, "gen"],
-			 			"target": [target_node.name, "gen"]
-			 		})
-
-				layer_links.append(node_links)
-		
-			link_data.append(layer_links)
-
-	return link_data
+	return [[[{"origin": [origin_node.name, "gen"], "target": [target_node.name, "gen"]} 
+	for origin_node in nodenet.layers[layer_index-1]] for target_node in layer] 
+	for layer_index, layer in enumerate(nodenet.layers) if 0 < layer_index < len(nodenet.layers)]
 
 def link_nodes(nodenet, link_data):
-
-	for layer in link_data:
-		layer_links = []
-
-		for node in layer:
-			node_links = []
-
-			for link in node:
-				node_links.append(create_link(nodenet, link))
-
-			layer_links.append(node_links)
-
-		nodenet.links_list.append(layer_links)
+	nodenet.links_list = [[[(create_link(nodenet, link)) 
+	for link in node] for node in layer] for layer in link_data]
 
 def create_link(nodenet, link_data):
-
 	origin_node = nodenet.node_dict[link_data.get("origin")[0]]
 	origin_gate = origin_node.get_gate(link_data.get("origin")[1])
 	target_node = nodenet.node_dict[link_data.get("target")[0]]
@@ -76,14 +39,11 @@ def create_link(nodenet, link_data):
 # PREPARE DATA AND SET ACTIVATION
 
 def _flatten_image(image):
-	flattened = []
-	for row in image:
-		for pixel in row:
-			flattened.append(pixel)
-	return flattened
+	return [pixel for row in image for pixel in row]
 			
 def set_activation(nodenet, image):
 	flattened_image = _flatten_image(image)
+
 	activation = []
 
 	for i, node in enumerate(nodenet.layers[0]):
@@ -101,17 +61,13 @@ def set_activation(nodenet, image):
 # UPDATE LINK WEIGHTS
 
 def update_weights(nodenet, activation, error_array, i):
-
 	output_links = nodenet.links_list[len(nodenet.layers)-2]
 	INITIAL_LEARNING_RATE = .05
 	RATE_DECAY = .0001
 	global learning_rate
 
 	# set and decay learning rate 
-	if i == 0:
-		learning_rate = INITIAL_LEARNING_RATE
-	else:
-		learning_rate = _decay_learning_rate(learning_rate, RATE_DECAY)
+	learning_rate = INITIAL_LEARNING_RATE if i == 0 else _decay_learning_rate(learning_rate, RATE_DECAY)
 		
 	# set weights for each link to output nodes based on pixel value
 	for node_index, output_node in enumerate(output_links):
