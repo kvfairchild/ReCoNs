06fd300be98bf1687af87130b1524553a805f5a2
kgallagher <katherinevgallagher@gmail.com>
2018-01-14 18:13:44 -0500

    division fix

--- nodenet/stack.py
+++ nodenet/stack.py
@@ -7,7 +7,7 @@ class Stack:
     def push(self, item):
     	self.items.append(item)
 
-    	if item in ("+", "-", "*", "/") and len(self.items) >= 3:
+    	if item in ("+", "-", "*", "\\") and len(self.items) >= 3:
     		self._calculate()
 
     def pop(self):
@@ -15,8 +15,9 @@ class Stack:
 
     def _calculate(self):
     	op, digit2, digit1 = self.pop(), self.pop(), self.pop()
+        print "op: ", op, "digit2: ", digit2, "digit1: ", digit1
 
-        if not any((isinstance(digit1, numbers.Number), isinstance(digit2, numbers.Number))):
+        if not all((isinstance(digit1, numbers.Number), isinstance(digit2, numbers.Number))):
             raise ValueError("incorrect values on stack for function calculation")
             return
     	
